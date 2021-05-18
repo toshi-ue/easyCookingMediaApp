e28005f5682e843b95c0f48d6fbb3f27f18304b5
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2021-05-18 18:40:31 +0900

    update: create, store, show, update, destroyアクションを追加

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -14,10 +14,47 @@ class RecipeController extends Controller
         return view('recipe/index', compact('recipes'));
     }
 
+    public function create()
+    {
+        $recipe = new Recipe();
+
+        return view('recipe/create', compact('recipe'));
+    }
+
+    public function store()
+    {
+        return view('recipe/edit', compact('recipe'));
+    }
+
+    public function show($id)
+    {
+        $recipe = Recipe::findOrFail($id);
+
+        return view('recipe/show', compact('recipe'));
+    }
+
     public function edit($id)
     {
         $recipe = Recipe::findOrFail($id);
 
         return view('recipe/edit', compact('recipe'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $recipe = Recipe::findOrFail($id);
+        $recipe->name = $request->name;
+        $recipe->cookingtime = $request->cookingtime;
+        $recipe->save();
+
+        return redirect("/recipe");
+    }
+
+    public function destroy($id)
+    {
+        $recipe = Recipe::findOrFail($id);
+        $recipe->delete();
+
+        return redirect("/recipe");
     }
 }
