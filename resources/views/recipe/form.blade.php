1f0d524b486ae09c8f4f3c19414c8419754af442
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2021-05-21 12:01:52 +0900

    update: cookingtoolsのcheckbox、is_publishedのセレクトボックスを追加

--- resources/views/recipe/form.blade.php
+++ resources/views/recipe/form.blade.php
@@ -16,12 +16,19 @@
       <input type="hidden" name="_token" value="{{ csrf_token() }}">
       <div class="form-group">
         <label for="name">レシピ名</label>
-        <input type="text" class="form-control" name="name" value="{{ $recipe->name }}">
+        <input type="text" class="form-control" name="name" value="{{ old('name', $recipe->name) }}">
         @if ($errors->has('name'))
           @foreach ($errors->get('name') as $message)
             <span class="text-danger">{{ $message }}</span><br>
           @endforeach
         @endif
+      </div>
+      <!-- TODO ファイルアップロード機能を実装する-->
+      <label>ファイルアップロード(未実装)</label>
+      <div class="form-group">
+        <div class="custom-file">
+          <input type="file" class="custome-file-input" id="inputFile">
+        </div>
       </div>
       <div class="form-group">
         <label for="cookingtime">所要時間</label>
@@ -34,36 +41,54 @@
       </div>
       <div class="form-group">
         <label for="description">説明</label>
-        <textarea class="form-control   "name="description" cols="30" rows="4" value="{{ $recipe->description }}"></textarea>
+        <textarea class="form-control" name="description" cols="30" rows="4">{{ $recipe->description }}</textarea>
         @if ($errors->has('description'))
           @foreach ($errors->get('description') as $message)
             <span class="text-danger">{{ $message }}</span><br>
           @endforeach
         @endif
       </div>
-      <!-- TODO toolsテーブルを作成した後に追加する
-      道具(tools, checkbox)
+
+
+      <!-- TODO
+        複数のcheckboxの値を保存できるようにする
+        あとでtoolsのcrudを作成して複数参照、表示する
+      -->
       <div class="form-group">
-        <label for="cookingtime">所要時間</label>
-        <input type="number" class="form-control" name="cookingtime" value="{{ $recipe->cookingtime }}">
-        @if ($errors->has('cookingtime'))
-          @foreach ($errors->get('cookingtime') as $message)
-            <span class="text-danger">{{ $message }}</span><br>
-          @endforeach
-        @endif
+      <label>調理道具</label>
+      <!--
+      @foreach($cookingtools as $index => $cookingtool)
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="checkbox" name="cookingtools[]" id="inlineCheckbox{{ $index }}" value="{{ $index }}">
+          <label class="form-check-label">{{$cookingtool}}</label>
+        </div>
+      @endforeach
+      -->
+      @foreach($cookingtools as $id => $cookingtool)
+        <div class="form-check form-check-inline">
+          <input class="form-check-input" type="checkbox" name="cookingtool[]" id="cookingtool{{$id}}" value="{{ $id }}"
+            @if($errors->get('cookingtools'))
+              {{ is_arrary(old('cookingtool')) && array_keys(old('cookingtool'), $id) ? 'checked="checked"' : ''}}
+            @else
+              {{ in_array($id, $cookingtools, true) ? 'checke="checked"' : ''}}
+            @endif
+          >
+          <br>
+          <label class="form-check-label">{{$cookingtool}}</label>
+        </div>
+      @endforeach
       </div>
-       -->
       <div class="form-group">
-        <label for="is_comment_allowed">コメント許可</label><br/>
-        <input type="radio" id="allowed" name="is_comment_allowed" value="1" checked="{{ $recipe->is_comment_allowed }}">
-        <label for="allowed">許可する</label>
-        <input type="radio" id="not_allowed" name="is_comment_allowed" value="0" checked="{{ $recipe->is_comment_allowed }}">
-        <label for="allowed">許可しない</label>
-        @if ($errors->has('is_comment_allowed'))
-          @foreach ($errors->get('is_comment_allowed') as $message)
-            <span class="text-danger">{{ $message }}</span><br>
-          @endforeach
-        @endif
+          <label for="is_comment_allowed">コメント許可</label><br />
+          <input type="radio" name="is_comment_allowed" value="1" @if (old('is_comment_allowed', $recipe->is_comment_allowed) == 1) checked @endif>
+          <label for="allowed">許可する</label>
+          <input type="radio" name="is_comment_allowed" value="0" @if (old('is_comment_allowed', $recipe->is_comment_allowed ) == 0) checked @endif>
+          <label for="allowed">許可しない</label>
+          @if ($errors->has('is_comment_allowed'))
+            @foreach ($errors->get('is_comment_allowed') as $message)
+              <span class="text-danger">{{ $message }}</span><br>
+            @endforeach
+          @endif
       </div>
       <!-- TODO カテゴリーテーブルを作成して値を参照するセレクトボックスを作成する
       カテゴリ(select_box)
@@ -77,20 +102,19 @@
         @endif
       </div>
       -->
-        <!-- TODO: 画像アップロード機能を追加する?
-        (file)
+      <!-- TODO: セレクトボックスを追加する -->
+      <label>公開状態</label>
       <div class="form-group">
-        <label for="cookingtime">所要時間</label>
-        <input type="number" class="form-control" name="cookingtime" value="{{ $recipe->cookingtime }}">
-        @if ($errors->has('cookingtime'))
-          @foreach ($errors->get('cookingtime') as $message)
-            <span class="text-danger">{{ $message }}</span><br>
-          @endforeach
-        @endif
+        <select class="form-control" name="is_published" id="is_published">
+            <option value="0" @if (old('is_published') == 0) selected @endif>公開しない</option>
+            <option value="1" @if (old('is_published') == 1) selected @endif>公開する</option>
+        </select>
       </div>
-        -->
       <button type="submit" class="btn btn-default">登録</button>
-      <input type="reset"  class="btn btn-default" value="リセット">
+        <!-- FIXME 初期化の方法が違う? -->
+        <!--
+        <input type="reset" class="btn btn-default" value="リセット">
+        -->
       <a href="/recipe">戻る</a>
       </form>
     </div>
